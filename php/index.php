d7ba70769ed89dc1e629a7cd742d0acac969e558
Jason McGuirk <jason.mcguirk@gmail.com>
2012-11-14 16:17:23 -0800

    Flesh out node and netty benchmarks

--- php/index.php
+++ php/index.php
@@ -2,8 +2,6 @@
 
 require 'predis/lib/Predis.php';
 
-$document = json_decode(file_get_contents("../util/document.json"), true);
-
 // simple set and get scenario
 
 $single_server = array(
@@ -15,8 +13,16 @@ $single_server = array(
 $redis = new Predis_Client($single_server);
 
 $retval = $redis->get('user_data');
-$document = json_decode(gzuncompress($retval), true);
-echo $document['TWIDDLE'] . "\n";
+$raw = gzuncompress($retval);
+$document = json_decode($raw, true);
 $document['TWIDDLE'] = mt_rand(0, 10000);
+for($i = 0; $i < 100; $i++){
+	for($j = 0; $j < 100; $j++){
+		$k = sin($i) * tan($j);
+	}
+}
+//echo "Len: " . strlen($raw) . " new val : " . $document['TWIDDLE'];
+//print_r(json_encode($document));
 $retval = $redis->set('user_data', gzcompress(json_encode($document)));
-print_r($retval);
+echo "OK\n";
+//print_r($retval);
--- util/load_doc.php
+++ util/load_doc.php
@@ -9,11 +9,12 @@ $document = json_decode(file_get_contents("document.json"), true);
 $single_server = array(
     'host'     => '10.174.178.235',
     'port'     => 6379,
-    'database' => 15
+    'database' => 0
 );
 
 $redis = new Predis_Client($single_server);
 
+//print_r($document);
 
-$redis->set('user_data', gzcompress(json_encode($document)));
+$redis->set('user_data', json_encode($document));
 //print_r($retval);
